0ecd92df7db44c53a41a4a2dac12dd7862200aee
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2024-04-09 03:23:54 +0300

    Add MainView

--- TicTacToe/MainView.swift
+++ TicTacToe/MainView.swift
@@ -7,18 +7,47 @@
 
 import SwiftUI
 
-struct MainView: View {
+struct MainView<ViewModel: MainViewModelProtocol>: View {
+    @StateObject
+    private var viewModel: ViewModel
     
-    @State private var isLoading = false
+    // MARK: - Initializing View
+
+    init() where ViewModel == MainViewModel {
+        _viewModel = StateObject(
+            wrappedValue: MainViewModel()
+        )
+    }
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            ForEach(0 ..< 3, id:\.self) { row in
+                HStack(spacing: 20) {
+                    ForEach(0 ..< 3, id:\.self) { column in
+                        Button(action: {
+                            viewModel.ticTacToeDict["\(row)\(column)"] = "multiply"
+                            viewModel.nextStep()
+                        }) {
+                            Image(systemName: viewModel.ticTacToeDict["\(row)\(column)"] ?? "")
+                                .imageScale(.large)
+                                .foregroundColor(.accentColor)
+                                .frame(width: 50, height: 50)
+                                .background(Color.yellow)
+                        }
+                        .alert(isPresented: $viewModel.showingWinner) {
+                            Alert(
+                                title: Text("Победили \(viewModel.sign)!!!"),
+                                message: Text("Сыграем еще?"),
+                                primaryButton: .default(Text("ОК")) {
+                                    viewModel.ticTacToeDict = [:]
+                                },
+                                secondaryButton: .cancel()
+                            )
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
